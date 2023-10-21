f0011063f4979bdb7bb87ed6ea0935349432647f
Joanna Graczyk <joanna.graczyk1@gmail.com>
2023-10-21 18:19:31 +0200

    Add cleanup to webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  // ENTRY
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  // PLUGINS
+  plugins: [new HtmlWebpackPlugin({ title: "Slot Game" })],
+  // OTPUT
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  // MODULE
   module: {
     rules: [
       {
